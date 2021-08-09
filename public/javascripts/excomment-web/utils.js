bef248d070773e312c16e6bc843faabca1809124
diogoloiola <diogomouraloiola@gmail.com>
2021-08-09 10:01:49 -0300

    Mudada a forma de como as querys eram geradas, removido funçõesrelacionadas ao gráfico de barra

--- public/javascripts/excomment-web/utils.js
+++ public/javascripts/excomment-web/utils.js
@@ -8,105 +8,22 @@ export function getDataWithAxios(url) {
     return axios.get(url)
 }
 
-function formatUrlToGetTheAmountTD(attribute, id) {
-    if (attribute === 'with-heuristics')
-        return `${URL_API}/dt/project/${id}/database/with`
+function formatUrlToGet(typeUrl, id) {
+    if (typeUrl === 1)
+        return `${URL_API}/jsonHierarchical/${id}/database/with?flag=true`
+    else if (typeUrl === 2)
+        return `${URL_API}/jsonHierarchical/${id}/database/with?flag=false`
 }
 
-export function formatQueryTheAmountTD(inputs) {
+export function formatQuery(typeUrl, inputs) {
 
     let axiosConsult = []
 
     inputs.forEach(input => {
-        const typeRepository = input.getAttribute('type-repository')
         const value = input.value
-        const urlApi = formatUrlToGetTheAmountTD(typeRepository, value)
+        const urlApi = formatUrlToGet(typeUrl, value)
         axiosConsult.push(getDataWithAxios(urlApi))
     })
 
     return axiosConsult
-}
-
-
-export function technicalDebtForIndex(tdType) {
-    switch (tdType) {
-        case 'architecture debt':
-            return 0
-            break;
-        case 'build debt':
-            return 1;
-            break;
-        case 'code debt':
-            return 2;
-            break;
-        case 'requirement debt':
-            return 3
-            break;
-        case 'defect debt':
-            return 4
-            break;
-        case 'design debt':
-            return 5;
-            break;
-        case 'documentation debt':
-            return 6;
-            break;
-        case 'test debt':
-            return 7;
-            break;
-    }
-}
-
-export function factoryDataBarChart(data) {
-    return {
-        chart: {
-            type: 'column'
-        },
-        xAxis: {
-            categories: ['', 'DT1', 'DT2', 'DT3', 'DT4', 'DT4', 'DT5', 'DT6', 'DT7', 'DT8', 'DT9']
-        },
-        title: {
-            text: ''
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'Quantidade de dividas Apache Ant'
-            },
-            stackLabels: {
-                enabled: true,
-                style: {
-                    fontWeight: 'bold',
-                    color: ( // theme
-                        Highcharts.defaultOptions.title.style &&
-                        Highcharts.defaultOptions.title.style.color
-                    ) || 'gray'
-                }
-            }
-        },
-        legend: {
-            align: 'center',
-            x: 0,
-            verticalAlign: 'bottom',
-            y: 10,
-            floating: true,
-            backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || 'white',
-            borderColor: '#CCC',
-            borderWidth: 1,
-            shadow: false
-        },
-        tooltip: {
-            headerFormat: '<b>{point.x}</b><br/>',
-            pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
-        },
-        plotOptions: {
-            column: {
-                stacking: 'normal',
-                dataLabels: {
-                    enabled: true
-                }
-            }
-        },
-        series: data
-    };
 }
