751f2d9cf4652a4cd83db8fafe64ef32b3fb372b
Diogoloiola <diogomouraloiola@gmail.com>
2021-09-20 20:45:41 -0300

    Criada funções auxiliares que vaõa ajudar na construção do gráfico  de barra

--- public/javascripts/excomment-web/utils.js
+++ public/javascripts/excomment-web/utils.js
@@ -17,6 +17,8 @@ function formatUrlToGet(typeUrl, id) {
         return `${URL_API}/jsonHierarchical/${id}/database/with?flag=true&colors=true`
     else if (typeUrl === 4)
         return `${URL_API}/jsonHierarchical/${id}/database/with?flag=false&type=2`
+    else if (typeUrl === 5)
+        return `${URL_API}/dt/project/${id}/database/with`
 }
 
 export function formatQuery(typeUrl, inputs) {
@@ -62,5 +64,88 @@ export function createLegendForGraphic(idContainer) {
 
         container.appendChild(flexIntens)
 
+    }
+}
+
+export function factoryDataBarChart(data) {
+    return {
+        chart: {
+            type: 'column'
+        },
+        xAxis: {
+            categories: ['', 'DT1', 'DT2', 'DT3', 'DT4', 'DT4', 'DT5', 'DT6', 'DT7', 'DT8', 'DT9']
+        },
+        title: {
+            text: ''
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Quantidade de dividas Apache Ant'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: ( // theme
+                        Highcharts.defaultOptions.title.style &&
+                        Highcharts.defaultOptions.title.style.color
+                    ) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'center',
+            x: 0,
+            verticalAlign: 'bottom',
+            y: 10,
+            floating: true,
+            backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            headerFormat: '<b>{point.x}</b><br/>',
+            pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        series: data
+    };
+}
+
+export function technicalDebtForIndex(tdType) {
+    switch (tdType) {
+        case 'architecture debt':
+            return 0
+            break;
+        case 'build debt':
+            return 1;
+            break;
+        case 'code debt':
+            return 2;
+            break;
+        case 'requirement debt':
+            return 3
+            break;
+        case 'defect debt':
+            return 4
+            break;
+        case 'design debt':
+            return 5;
+            break;
+        case 'documentation debt':
+            return 6;
+            break;
+        case 'test debt':
+            return 7;
+            break;
     }
 }
