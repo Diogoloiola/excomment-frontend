22bc3fdaadd46a14db6d7cda1b36e6576d498da4
Diogoloiola <diogomouraloiola@gmail.com>
2021-12-11 21:14:00 -0300

    permitindo que o usuário baixe qualquer repositório

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,30 +1,46 @@
+import { useState } from 'react'
 import { Table as TableStyled, Select } from './style'
 
-function formatUrl(url, tagName) {
-    return `${url}/archive/refs/tags/${tagName}.zip`
+function formatUrl(url, tagName, type = 1) {
+    if(type == 1)
+        return `${url}/archive/refs/tags/${tagName}.zip`
+    else if(type == 2)
+        return `${url}/archive/refs/heads/main.zip`
 }
 
 function createOptionTable(releases, url) {
     if (releases.length) {
-        let flag = 0
         return releases.map((release, j) => {
-            if (!flag) {
-                flag = 1;
+            if (release.name) {
                 return (
-                    <option key={j + 10} value={''}>-</option>
-                )
-            }
-            if (release.name && flag) {
-                return (
-                    <option key={j + 10} value={formatUrl(url, release.name)}>{release.name}</option>
+                    <option key={j + 10} value={formatUrl(url, release.tag_name)}>{release.name}</option>
                 )
             }
         })
     }
-    return <option value=''>Sem releases</option>
+    return <option value={formatUrl(url, '', 2)}>Master</option>
 }
 
+function getInitialValues(data) {
+    const result = []
+
+    data.forEach(element => {
+        const name = element.name
+        result.push({[name]: null})
+    })
+    return result
+}
+
+function handleSelectChange(urls, setUrls, name, url){
+    setUrls({
+        ...urls,
+        [name]: url
+    })
+}
 export default function Table({ data }) {
+
+    const [urls, setUrls] = useState(getInitialValues(data))
+
     return (
         <TableStyled>
             <thead>
@@ -43,12 +59,13 @@ export default function Table({ data }) {
                                 <th>{index + 1}</th>
                                 <th>{element.name}</th>
                                 <th>
-                                    <Select>
-                                        {createOptionTable(element.releases, element.url)}
+                                    <Select onChange={(e) => handleSelectChange(urls, setUrls, element.name, e.target.value)}>
+                                        <option value="">-</option>
+                                        {createOptionTable(element.releases, element.url, setUrls, element.url)}
                                     </Select>
                                 </th>
                                 <th>
-                                    Download
+                                    <a href={urls[`${element.name}`]}>{element.name}</a>
                                 </th>
                             </tr>
                         )
