dc7b7f1bd4eacf9ad4ca100eb291a4290c779f6d
Diogoloiola <diogomouraloiola@gmail.com>
2021-12-12 12:05:10 -0300

    adicionando o componente chart no componente excomment

--- src/components/excomment/index.jsx
+++ src/components/excomment/index.jsx
@@ -1,29 +1,38 @@
 import { useState } from 'react'
-import { Container, Button, Header } from './style.js'
+import { Container, Button, Header, Content } from './style.js'
 import ModalApplication from '../modal'
 import AnyChart from 'anychart-react'
 import anychart from 'anychart'
+import ChartAnychart from './../chart'
+
+function renderChart(data, chartType) {
+    if (data.name === 'home') {
+        const stage = anychart.graphics.create();
+        const chart = new ChartAnychart(parseInt(chartType), data, anychart)
+        return (<AnyChart
+            instance={stage}
+            width={'100%'}
+            height={700}
+            charts={[chart.typeGraphic()]}
+        />)
+    }
+}
 
 export default function Excomment() {
 
     const [show, setShow] = useState(false)
-    const [chart, setChart] = useState([]);
-    let stage = anychart.graphics.create();
+    const [data, setData] = useState([]);
+    const [chartType, setChartType] = useState(null)
 
     return (
         <Container>
             <Header>
                 <Button onClick={() => setShow(true)}>Selecionar projeto</Button>
             </Header>
-            <ModalApplication show={show} setShow={setShow} setChart={setChart} />
-            {/* <div style={{ width: '100%', height: '500px' }}>
-                <AnyChart
-                    instance={stage}
-                    width={800}
-                    height={600}
-                    charts={chart}
-                />
-            </div> */}
+            <ModalApplication show={show} setShow={setShow} setChartData={setData} setChartTypeGraph={setChartType} />
+            <Content>
+                {renderChart(data, chartType)}
+            </Content>
         </Container>
     )
 }
--- src/components/excomment/style.js
+++ src/components/excomment/style.js
@@ -2,12 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.section `
   width: 100%;
+  height: 80%;
 `
 
 export const Header = styled.header `
   width: 100%;
   margin-top: 20px;
   box-sizing: border-box;
+`
+
+export const Content = styled.div `
+  width: 90%;
+  height: 90%;
+  margin: auto;
+  background: #fff;
+  margin-top: 10px;
 `
 
 export const Button = styled.button `
