d884cfac978b342e80be6a1665a6e91e722fb0c6
Diogoloiola <diogomouraloiola@gmail.com>
2021-12-11 21:22:05 -0300

    ajustando os links da aplicação

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -5,42 +5,46 @@ import Home from '../Home';
 import Gift from '../Gift'
 import { TableRepositories } from '../Gift/table';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 export default function Layout() {
     return (
         <Container>
-            <LeftSide>
-                <ul>
-                    <li>
-
-                    </li>
-                    <li>
-                        <ContainerIcon>
-                            <FaHome />
-                        </ContainerIcon>
-                    </li>
-                    <li>
-                        <ContainerIcon>
-                            <FaChartBar />
-                        </ContainerIcon>
-                    </li>
-                    <li>
-                        <ContainerIcon>
-                            <FaGithub />
-                        </ContainerIcon>
-                    </li>
-                    <li></li>
-                </ul>
-            </LeftSide>
-            <RightSide>
-                <Header />
-                <Router>
+            <Router>
+                <LeftSide>
+                    <ul>
+                        <li></li>
+                        <li>
+                            <Link to="/">
+                                <ContainerIcon>
+                                    <FaHome />
+                                </ContainerIcon>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/excomment-web">
+                                <ContainerIcon>
+                                    <FaChartBar />
+                                </ContainerIcon>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/gift">
+                                <ContainerIcon>
+                                    <FaGithub />
+                                </ContainerIcon>
+                            </Link>
+                        </li>
+                        <li></li>
+                    </ul>
+                </LeftSide>
+                <RightSide>
+                    <Header />
                     <Route path="/" exact render={() => <Home />} />
                     <Route path="/gift" exact component={() => <Gift />} />
                     <Route path="/repositories" exact component={() => <TableRepositories />} />
-                </Router>
-            </RightSide>
+                </RightSide>
+            </Router>
         </Container>
     )
 }
