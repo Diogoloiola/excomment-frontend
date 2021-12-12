349219db8f8ba5e031c89a34ddefb6413ffd6705
Diogoloiola <diogomouraloiola@gmail.com>
2021-12-12 11:19:34 -0300

    adicionando novos métodos para consultar api da excomment

--- src/models/excomment-api.js
+++ src/models/excomment-api.js
@@ -12,4 +12,26 @@ export default class ExcommentApi {
             return error
         }
     }
+
+    async getHierarchicalJson(typeUrl, id) {
+        try {
+            const { data } = await this.axios.get(this.formatUrlToGet(parseInt(typeUrl), id))
+            return data
+        } catch (error) {
+            return error;
+        }
+    }
+
+    formatUrlToGet(typeUrl, id) {
+        if (typeUrl === 1)
+            return `${this.url}/jsonHierarchical/${id}/database/with?flag=false`
+        else if (typeUrl === 2)
+            return `${this.url}/jsonHierarchical/${id}/database/with?flag=true`
+        else if (typeUrl === 3)
+            return `${this.url}/jsonHierarchical/${id}/database/with?flag=true&colors=true`
+        else if (typeUrl === 4)
+            return `${this.url}/jsonHierarchical/${id}/database/with?flag=false&type=2`
+        else if (typeUrl === 5 || typeUrl === 6)
+            return `${this.url}/dt/project/${id}/database/with`
+    }
 }
